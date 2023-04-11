83395cb62d88d02f7a68b55ff45dd22186ab6d0b
Emanuel Assuero <63565141+Assu3ro@users.noreply.github.com>
2023-04-11 15:13:20 -0300

    Inicio do Quiz
    Códigos iniciais.

---
+++ quiz.py
@@ -0,0 +1,14 @@
+print("Seja bem vindo ao QUIZ")
+    answer_user = input("Quer começar? (S/N)")
+
+if answer_user != "S":
+    quit()
+
+print("Começando...")
+print("Quem fez o avião? /n (A) Santos Dumont /n (B)Leonardo da Vinci /n (C)Charlie Charplin /n (D)Will Smith")
+answer_1 input("Resposta: ")
+
+if answer_1 ++ "A":
+    print("Correto")
+else:
+    print("Incorreto")
